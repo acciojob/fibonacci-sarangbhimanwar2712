7e4eb5664915c9e68072e9d605d664305f6fd8b0
Sarang Sanjay Bhimanwar <117634221+sarangbhimanwar2712@users.noreply.github.com>
2023-04-11 22:08:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
 // your code here
+	if (num === 0) {
+    return 0;
+  } else if (num === 1 || num === 2) {
+    return 1;
+  } else {
+    let fib = [0, 1, 1];
+    for (let i = 3; i <= num; i++) {
+      fib[i] = fib[i-1] + fib[i-2];
+    }
+    return fib[num];
+  }
 }
 
 module.exports = fibonacci;
